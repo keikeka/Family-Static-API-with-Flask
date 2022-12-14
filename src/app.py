c121069e7e19c475302867939f4481bf0f1937e2
keikeka <keilirosalesr@gmail.com>
2022-12-14 22:34:17 +0000

    Created endpoints to manage family structure

--- src/app.py
+++ src/app.py
@@ -1,12 +1,8 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -25,18 +21,44 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# get all family members
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+# retrieve one member
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_single_member(member_id):
+    memberId = jackson_family.get_member(member_id)
+    return jsonify(memberId), 200
+
+# add new member
+@app.route('/member', methods=['POST'])
+def post_one_member():
+    body_id = request.json.get("id")
+    body_first_name = request.json.get("first_name")
+    body_last_name = request.json.get("last_name")
+    body_age = request.json.get("age")
+    body_lucky_numbers = request.json.get("lucky_numbers")
+
+    memberAdd = {
+        "id": body_id or jackson_family._generateId(),
+        "first_name": body_first_name,
+        "last_name": body_last_name,
+        "age": body_age,
+        "lucky_numbers": body_lucky_numbers,
+    }
+    jackson_family.add_member(memberAdd)
+    return jsonify(None), 200
+
+# delete one member
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_single_member(member_id):
+    memberId = jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
